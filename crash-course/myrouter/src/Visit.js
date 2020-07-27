e7f6299b768a1eb1612c3df887ade5298105d1f7
nitish <nitishp74@gmail.com>
2020-07-27 12:44:54 +0530

    Done With React CrashCOurse

--- crash-course/myrouter/src/Visit.js
+++ crash-course/myrouter/src/Visit.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App(){
   return (
     <div>
-      <h1>App Component</h1>
+      <h1>Visit Component</h1>
     </div>
   )
 }
--- crash-course/myrouter/src/index.js
+++ crash-course/myrouter/src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import User from './User';
+import Visit from './Visit';
+import NotFound from './notfound';
+import { Route, Link, Switch, BrowserRouter as Router } from "react-router-dom";
 
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/User">User</Link></li>
+        <li><Link to="/Visit">Visit</Link></li>
+        
+      </ul>
+    </div>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/user" component={User} />
+      <Route path="/Visit" component={Visit} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
